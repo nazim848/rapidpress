a173824d0bdfb4f959093a4d0ed1e922080e4c68
Nazim Husain <28640245+nazim848@users.noreply.github.com>
2024-08-19 16:56:23 +0530

    updated fields

--- admin/partials/tabs/general.php
+++ admin/partials/tabs/general.php
@@ -2,6 +2,8 @@
 // Ensure this file is being included by a parent file
 if (!defined('ABSPATH')) exit; // Exit if accessed directly
 
+use RapidPress\RP_Options;
+
 ?>
 
 <div id="<?php echo esc_attr($tab_id); ?>" class="tab-pane">
@@ -13,7 +15,7 @@ if (!defined('ABSPATH')) exit; // Exit if accessed directly
 				<td>
 					<div class="checkbox-radio">
 						<label>
-							<input type="checkbox" name="rapidpress_disable_comments" value="1" <?php checked(1, get_option('rapidpress_disable_comments'), true); ?> />
+							<input type="checkbox" name="rapidpress_options[disable_comments]" value="1" <?php checked(1, RP_Options::get_option('disable_comments'), true); ?> />
 						</label>
 						<span class="dashicons dashicons-editor-help" title="Disable comments for all posts"></span>
 					</div>
@@ -24,7 +26,7 @@ if (!defined('ABSPATH')) exit; // Exit if accessed directly
 				<td>
 					<div class="checkbox-radio">
 						<label>
-							<input type="checkbox" name="rapidpress_remove_comment_urls" value="1" <?php checked(1, get_option('rapidpress_remove_comment_urls'), true); ?> />
+							<input type="checkbox" name="rapidpress_options[remove_comment_urls]" value="1" <?php checked(1, RP_Options::get_option('remove_comment_urls'), true); ?> />
 						</label>
 						<span class="dashicons dashicons-editor-help" title="Remove comment URLs from the header"></span>
 					</div>
@@ -35,7 +37,7 @@ if (!defined('ABSPATH')) exit; // Exit if accessed directly
 				<td>
 					<div class="checkbox-radio">
 						<label>
-							<input type="checkbox" name="rapidpress_disable_dashicons" value="1" <?php checked(1, get_option('rapidpress_disable_dashicons'), true); ?> />
+							<input type="checkbox" name="rapidpress_options[disable_dashicons]" value="1" <?php checked(1, RP_Options::get_option('disable_dashicons'), true); ?> />
 						</label>
 						<span class="dashicons dashicons-editor-help" title="Disable Dashicons"></span>
 					</div>
@@ -46,7 +48,7 @@ if (!defined('ABSPATH')) exit; // Exit if accessed directly
 				<td>
 					<div class="checkbox-radio">
 						<label>
-							<input type="checkbox" name="rapidpress_disable_embeds" value="1" <?php checked(1, get_option('rapidpress_disable_embeds'), true); ?> />
+							<input type="checkbox" name="rapidpress_options[disable_embeds]" value="1" <?php checked(1, RP_Options::get_option('disable_embeds'), true); ?> />
 						</label>
 						<span class="dashicons dashicons-editor-help" title="Disable Embeds"></span>
 					</div>
@@ -57,7 +59,7 @@ if (!defined('ABSPATH')) exit; // Exit if accessed directly
 				<td>
 					<div class="checkbox-radio">
 						<label>
-							<input type="checkbox" name="rapidpress_disable_xmlrpc" value="1" <?php checked(1, get_option('rapidpress_disable_xmlrpc'), true); ?> />
+							<input type="checkbox" name="rapidpress_options[disable_xmlrpc]" value="1" <?php checked(1, RP_Options::get_option('disable_xmlrpc'), true); ?> />
 						</label>
 						<span class="dashicons dashicons-editor-help" title="Disable XML-RPC"></span>
 					</div>
@@ -68,7 +70,7 @@ if (!defined('ABSPATH')) exit; // Exit if accessed directly
 				<td>
 					<div class="checkbox-radio">
 						<label>
-							<input type="checkbox" name="rapidpress_disable_emojis" value="1" <?php checked(1, get_option('rapidpress_disable_emojis'), true); ?> />
+							<input type="checkbox" name="rapidpress_options[disable_emojis]" value="1" <?php checked(1, RP_Options::get_option('disable_emojis'), true); ?> />
 						</label>
 						<span class="dashicons dashicons-editor-help" title="Disable Emojis"></span>
 					</div>
@@ -79,7 +81,7 @@ if (!defined('ABSPATH')) exit; // Exit if accessed directly
 				<td>
 					<div class="checkbox-radio">
 						<label>
-							<input type="checkbox" name="rapidpress_remove_jquery_migrate" value="1" <?php checked(1, get_option('rapidpress_remove_jquery_migrate'), true); ?> />
+							<input type="checkbox" name="rapidpress_options[remove_jquery_migrate]" value="1" <?php checked(1, RP_Options::get_option('remove_jquery_migrate'), true); ?> />
 						</label>
 						<span class="dashicons dashicons-editor-help" title="Remove jQuery Migrate"></span>
 					</div>
@@ -90,7 +92,7 @@ if (!defined('ABSPATH')) exit; // Exit if accessed directly
 				<td>
 					<div class="checkbox-radio">
 						<label>
-							<input type="checkbox" name="rapidpress_disable_rss_feeds" value="1" <?php checked(1, get_option('rapidpress_disable_rss_feeds'), true); ?> />
+							<input type="checkbox" name="rapidpress_options[disable_rss_feeds]" value="1" <?php checked(1, RP_Options::get_option('disable_rss_feeds'), true); ?> />
 						</label>
 						<span class="dashicons dashicons-editor-help" title="Disable RSS Feeds"></span>
 					</div>
@@ -101,7 +103,7 @@ if (!defined('ABSPATH')) exit; // Exit if accessed directly
 				<td>
 					<div class="checkbox-radio">
 						<label>
-							<input type="checkbox" name="rapidpress_disable_rss_feed_links" value="1" <?php checked(1, get_option('rapidpress_disable_rss_feed_links'), true); ?> />
+							<input type="checkbox" name="rapidpress_options[disable_rss_feed_links]" value="1" <?php checked(1, RP_Options::get_option('disable_rss_feed_links'), true); ?> />
 						</label>
 						<span class="dashicons dashicons-editor-help" title="Disable RSS Feed Links"></span>
 					</div>
@@ -112,7 +114,7 @@ if (!defined('ABSPATH')) exit; // Exit if accessed directly
 				<td>
 					<div class="checkbox-radio">
 						<label>
-							<input type="checkbox" name="rapidpress_hide_wp_version" value="1" <?php checked(1, get_option('rapidpress_hide_wp_version'), true); ?> />
+							<input type="checkbox" name="rapidpress_options[hide_wp_version]" value="1" <?php checked(1, RP_Options::get_option('hide_wp_version'), true); ?> />
 						</label>
 						<span class="dashicons dashicons-editor-help" title="Hide WP Version"></span>
 					</div>
@@ -123,7 +125,7 @@ if (!defined('ABSPATH')) exit; // Exit if accessed directly
 				<td>
 					<div class="checkbox-radio">
 						<label>
-							<input type="checkbox" name="rapidpress_remove_global_styles" value="1" <?php checked(1, get_option('rapidpress_remove_global_styles'), true); ?> />
+							<input type="checkbox" name="rapidpress_options[remove_global_styles]" value="1" <?php checked(1, RP_Options::get_option('remove_global_styles'), true); ?> />
 						</label>
 						<span class="dashicons dashicons-editor-help" title="Remove Global Styles"></span>
 					</div>
@@ -134,7 +136,7 @@ if (!defined('ABSPATH')) exit; // Exit if accessed directly
 				<td>
 					<div class="checkbox-radio">
 						<label>
-							<input type="checkbox" name="rapidpress_disable_self_pingbacks" value="1" <?php checked(1, get_option('rapidpress_disable_self_pingbacks'), true); ?> />
+							<input type="checkbox" name="rapidpress_options[disable_self_pingbacks]" value="1" <?php checked(1, RP_Options::get_option('disable_self_pingbacks'), true); ?> />
 						</label>
 						<span class="dashicons dashicons-editor-help" title="Disable Self Pingbacks"></span>
 					</div>
@@ -145,7 +147,7 @@ if (!defined('ABSPATH')) exit; // Exit if accessed directly
 				<td>
 					<div class="checkbox-radio">
 						<label>
-							<input type="checkbox" name="rapidpress_disable_google_maps" value="1" <?php checked(1, get_option('rapidpress_disable_google_maps'), true); ?> />
+							<input type="checkbox" name="rapidpress_options[disable_google_maps]" value="1" <?php checked(1, RP_Options::get_option('disable_google_maps'), true); ?> />
 						</label>
 						<span class="dashicons dashicons-editor-help" title="Disable Google Maps"></span>
 					</div>
@@ -156,7 +158,7 @@ if (!defined('ABSPATH')) exit; // Exit if accessed directly
 				<td>
 					<div class="checkbox-radio">
 						<label>
-							<input type="checkbox" name="rapidpress_remove_rsd_link" value="1" <?php checked(1, get_option('rapidpress_remove_rsd_link'), true); ?> />
+							<input type="checkbox" name="rapidpress_options[remove_rsd_link]" value="1" <?php checked(1, RP_Options::get_option('remove_rsd_link'), true); ?> />
 						</label>
 						<span class="dashicons dashicons-editor-help" title="Remove RSD Link"></span>
 					</div>
@@ -167,7 +169,7 @@ if (!defined('ABSPATH')) exit; // Exit if accessed directly
 				<td>
 					<div class="checkbox-radio">
 						<label>
-							<input type="checkbox" name="rapidpress_remove_shortlink" value="1" <?php checked(1, get_option('rapidpress_remove_shortlink'), true); ?> />
+							<input type="checkbox" name="rapidpress_options[remove_shortlink]" value="1" <?php checked(1, RP_Options::get_option('remove_shortlink'), true); ?> />
 						</label>
 						<span class="dashicons dashicons-editor-help" title="Remove Shortlink"></span>
 						</label>
@@ -179,8 +181,8 @@ if (!defined('ABSPATH')) exit; // Exit if accessed directly
 				<td>
 					<select name="rapidpress_disable_rest_api" id="rapidpress_disable_rest_api">
 						<option value>Default (Enabled)</option>
-						<option value="non_admins" <?php selected(get_option('rapidpress_disable_rest_api'), 'non_admins'); ?>>Disable for Non-Admins</option>
-						<option value="guest_users" <?php selected(get_option('rapidpress_disable_rest_api'), 'guest_users'); ?>>Disable for Guest Users</option>
+						<option value="non_admins" <?php selected(RP_Options::get_option('disable_rest_api'), 'non_admins'); ?>>Disable for Non-Admins</option>
+						<option value="guest_users" <?php selected(RP_Options::get_option('disable_rest_api'), 'guest_users'); ?>>Disable for Guest Users</option>
 					</select>
 					<span class="dashicons dashicons-editor-help" title="Disable Rest API"></span>
 				</td>
@@ -190,7 +192,7 @@ if (!defined('ABSPATH')) exit; // Exit if accessed directly
 				<td>
 					<div class="checkbox-radio">
 						<label>
-							<input type="checkbox" name="rapidpress_remove_rest_api_links" value="1" <?php checked(1, get_option('rapidpress_remove_rest_api_links'), true); ?> />
+							<input type="checkbox" name="rapidpress_options[remove_rest_api_links]" value="1" <?php checked(1, RP_Options::get_option('remove_rest_api_links'), true); ?> />
 						</label>
 						<span class="dashicons dashicons-editor-help" title="Remove REST API link tag from the header"></span>
 				</td>
@@ -200,8 +202,8 @@ if (!defined('ABSPATH')) exit; // Exit if accessed directly
 			<td>
 				<select name="rapidpress_limit_post_revisions" id="rapidpress_limit_post_revisions">
 					<option value>Default (Unlimited)</option>
-					<option value="disable_revisions" <?php selected(get_option('rapidpress_limit_post_revisions'), 'disable_revisions'); ?>>Disable Post Revisions</option>
-					<option value="custom_revisions" <?php selected(get_option('rapidpress_limit_post_revisions'), 'custom_revisions'); ?>>Custom Revisions</option>
+					<option value="disable_revisions" <?php selected(RP_Options::get_option('limit_post_revisions'), 'disable_revisions'); ?>>Disable Post Revisions</option>
+					<option value="custom_revisions" <?php selected(RP_Options::get_option('limit_post_revisions'), 'custom_revisions'); ?>>Custom Revisions</option>
 				</select>
 				<span class="dashicons dashicons-editor-help" title="Limit Post Revisions"></span>
 			</td>
